35eb40f9a1f08643d5b0555e542aee45699b9c0e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-14 20:36:57 +0600

    feat(db): create categories table with user relation and unique constraint

--- src/db/index.ts
+++ src/db/index.ts
@@ -43,7 +43,22 @@ export const initDB = async () => {
         );
     `);
 
- 
+        // 3. Categories table
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS categories (
+        id BIGSERIAL PRIMARY KEY,
+
+        user_id BIGINT NOT NULL 
+        REFERENCES users(id) ON DELETE CASCADE,
+
+        name VARCHAR(50) NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+        UNIQUE(user_id, name)
+        );
+    `);
+
 
         console.log("Database connected and tables initialized successfully!");
     } catch (error) {
